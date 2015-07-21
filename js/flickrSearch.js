121c1fb3ea000a8ac276908810fcba014140874f
elainapolson <elainapolson@gmail.com>
2015-07-21 16:18:43 -0400

    done without bonus

--- js/flickrSearch.js
+++ js/flickrSearch.js
@@ -1,8 +1,54 @@
-$(document).ready(function(){
-  
-  //write your solution here...
+var openURL = "https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=2fd41b49fedfd589dc265350521ab539&tags="
+var endURL = "&jsoncallback=?"
+
+
+
+
+function formatPhotos(photosJSON) {
+    var photo = photosJSON.photos.photo;
+    var id;
+    var serverId;
+    var secret;
+    var URI
+    var string = "";
+    for (var i=0; i < photo.length; i++) {
+        farm = photo[i].farm;
+        id = photo[i].id;
+        serverId = photo[i].server;
+        secret = photo[i].secret;
     
-});
+        URI = 'http://farm' + farm + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg'
+        string += '<img src="' + URI + '"</img>'
+    }
+    return string
+}
+
+
+function addPhotosToDOM(photosJSON) {
+    var newPhotos = formatPhotos(photosJSON);
+    $('#feed').html(newPhotos);
+}
+
+
+function fetchJSON() {
+  var input = $('#keyword').val();
+
+    $.ajax ({
+      url: openURL + input + endURL,
+      contentType: 'application/json',
+      dataType: 'jsonp'
+    })
+    .done(addPhotosToDOM);
+}
+
+
+
+
+
+
+
+
+
 
 /*
 
